1688164b72680571d3d8d08e082a14ae59e127d8
SangSir <github@sangsir.com>
2017-12-23 22:06:16 +0800

    Update

--- pages/info/help.js
+++ pages/info/help.js
@@ -5,11 +5,16 @@ Page({
     img1: "../../images/contents/1.png",
     img2: "../../images/contents/2.png",
   },
+  returnIndex: function () {
+    wx.navigateBack({
+      delta: 1
+    });
+  },
   onShareAppMessage: function () {
     return {
-      title: '使用帮助',
+      title: '文经课表',
       desc: '「文经课表」提供烟台大学文经学院在校生班级与教师课表及空闲教室查询服务',
-      path: '/pages/info/help'
+      path: '/pages/index/index'
     }
   },
   onLoad: function () {
